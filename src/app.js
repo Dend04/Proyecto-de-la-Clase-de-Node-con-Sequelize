90c239ba635ef718faa7a4b9e40734be2bfe70bd
Dend04 <enamoradodairon@gmail.com>
2024-11-21 16:52:56 -0500

    feat: add documentacion de Swagger, env y logos

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import Pregunta from "./models/pregunta_model.js";
 import Respuesta from "./models/respuesta_model.js";
 import Resultado from "./models/resultado_model.js";
 import Rutina from "./models/rutina_model.js";
+import express from 'express';
+import swaggerSetup from './config/swagger.js'; // Asegúrate de que la ruta sea correcta
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 //Relaciones
 Test.hasMany(Pregunta, { foreignKey: "testId" });
@@ -22,6 +28,12 @@ Resultado.belongsTo(Test, { foreignKey: "testId" });
 Resultado.hasOne(Rutina, { foreignKey: "resultadoId" });
 Rutina.belongsTo(Resultado, { foreignKey: "resultadoId" });
 
+
+const app = express();
+
+// Configurar Swagger
+swaggerSetup(app);
+
 //Sincronizacion Base de Datos
 sequelize
   .sync({ force: false })
@@ -30,4 +42,8 @@ sequelize
   })
   .catch((error) => {
     console.error("Error al sincronizar la base de datos:", error);
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${process.env.PORT}`);
   });
--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv'
 
-const sequelize = new Sequelize('Pruebas de tests', 'postgres', ' ', {
-  host: 'localhost', 
-  dialect: 'postgres', // Especifica que estás usando PostgreSQL u otra base de datos
+// Cargar las variables de entorno
+dotenv.config()
+
+const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD, {
+  host: process.env.HOST, 
+  dialect: process.env.DB_DIALECT, // Especifica que estás usando PostgreSQL u otra base de datos
   logging: false, // Desactiva el registro de SQL en la consola
 });
 
