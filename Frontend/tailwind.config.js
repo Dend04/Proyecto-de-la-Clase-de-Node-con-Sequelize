473b769180fbafa5435751a541f7252e9734a4db
Dend04 <enamoradodairon@gmail.com>
2024-12-08 19:20:13 -0500

    feat: carpetas y nuxt con tailwinds en el frontend

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{vue,js}",
+    "./pages/**/*.vue",
+    "./layouts/**/*.vue",
+    "./components/**/*.vue,js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- src/middleware/middleware.js
+++
@@ -1,5 +0,0 @@
-
-export const middleware = (req, res, next) => {
-    console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
-    next(); // Pasa el control al siguiente middleware
-  };
