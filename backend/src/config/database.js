115719b2588a7db0a947ec043db082edb89ef4d7
Dend04 <enamoradodairon@gmail.com>
2025-02-07 17:43:28 -0500

    recuperando database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-// Cargar las variables de entorno
-dotenv.config();
+// Cargar las variables de entorno según el entorno
+const env = process.env.NODE_ENV || "development"; // Usar 'development' por defecto
+const envFile = `.env.${env}`; // Cargar el archivo .env correspondiente
+dotenv.config({ path: envFile });
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -10,10 +12,17 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.HOST,
-    port: process.env.DB_PORT || 5432,
-    dialect: process.env.DB_DIALECT, // Especifica que estás usando PostgreSQL u otra base de datos
+    port: process.env.DB_PORT || 5432, // Puerto por defecto para PostgreSQL
+    dialect: process.env.DB_DIALECT,
     logging: false, // Desactiva el registro de SQL en la consola
+    dialectOptions:{
+      ssl: {
+        require: true, // Requerido para Supabase
+        rejectUnauthorized: false, // Necesario para Supabase
+      },
+    }
   }
 );
+
 
 export default sequelize;
